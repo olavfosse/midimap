4df6f60a826274682f890a36f3d758ba702b1901
Olav Fosse <mail@olavfosse.no>
2020-12-28 21:39:34 +0100

    Refactor midimap.go to use a []Mapping to determine which MIDI events map to which keypresses

--- midimap.go
+++ midimap.go
@@ -5,27 +5,11 @@ import (
 	"fmt"
 	"os"
 
+	"./lang"
 	"./press"
 	"github.com/micmonay/keybd_event"
 	"github.com/rakyll/portmidi"
 )
-
-// the corresponding keyCode of a midi event E with data1 field D, is data1KeyCode[D]
-var data1KeyCode = make(map[int64]int)
-
-func init() {
-	// left upper tom drum skin
-	data1KeyCode[48] = keybd_event.VK_E
-
-	// sharp drum skin
-	data1KeyCode[38] = keybd_event.VK_F
-
-	// floor tom drum skin
-	data1KeyCode[43] = keybd_event.VK_J
-
-	// right upper tom drum skin
-	data1KeyCode[45] = keybd_event.VK_I
-}
 
 func main() {
 	portmidi.Initialize()
@@ -82,6 +66,68 @@ func main() {
 	}
 	defer in.Close()
 
+	var mappings []lang.Mapping
+	mappings = append(mappings, lang.Mapping{
+		Matcher: lang.Matcher{
+			LeftComparison: lang.Comparison{
+				LeftOperand:  lang.Part1,
+				Operator:     lang.EqualToOperator,
+				RightOperand: 48,
+			},
+			RightComparison: lang.Comparison{
+				LeftOperand:  lang.Part2,
+				Operator:     lang.UnequalToOperator,
+				RightOperand: 64,
+			},
+		},
+		KeyCode: keybd_event.VK_E,
+	})
+	mappings = append(mappings, lang.Mapping{
+		Matcher: lang.Matcher{
+			LeftComparison: lang.Comparison{
+				LeftOperand:  lang.Part1,
+				Operator:     lang.EqualToOperator,
+				RightOperand: 38,
+			},
+			RightComparison: lang.Comparison{
+				LeftOperand:  lang.Part2,
+				Operator:     lang.UnequalToOperator,
+				RightOperand: 64,
+			},
+		},
+		KeyCode: keybd_event.VK_F,
+	})
+	mappings = append(mappings, lang.Mapping{
+		Matcher: lang.Matcher{
+			LeftComparison: lang.Comparison{
+				LeftOperand:  lang.Part1,
+				Operator:     lang.EqualToOperator,
+				RightOperand: 43,
+			},
+			RightComparison: lang.Comparison{
+				LeftOperand:  lang.Part2,
+				Operator:     lang.UnequalToOperator,
+				RightOperand: 64,
+			},
+		},
+		KeyCode: keybd_event.VK_J,
+	})
+	mappings = append(mappings, lang.Mapping{
+		Matcher: lang.Matcher{
+			LeftComparison: lang.Comparison{
+				LeftOperand:  lang.Part1,
+				Operator:     lang.EqualToOperator,
+				RightOperand: 45,
+			},
+			RightComparison: lang.Comparison{
+				LeftOperand:  lang.Part2,
+				Operator:     lang.UnequalToOperator,
+				RightOperand: 64,
+			},
+		},
+		KeyCode: keybd_event.VK_I,
+	})
+
 	for {
 		events, err := in.Read(1024)
 		if err != nil && err != portmidi.ErrSysExOverflow {
@@ -92,17 +138,52 @@ func main() {
 		}
 
 		for _, event := range events {
-			mapMidiEventToKeyPress(event)
+			mapMidiEventToKeyPress(mappings, event)
 		}
 	}
 }
 
-func mapMidiEventToKeyPress(e portmidi.Event) {
-	if e.Data2 == 64 { // ignore "finished hitting" event
-		return
+// evaluateComparison returns true if the comparison c evaluates to true in the context of event e in midimap-lang. Otherwise it returns false.
+func evaluateComparison(c lang.Comparison, e portmidi.Event) bool {
+	var data int64
+	if c.LeftOperand == lang.Part1 {
+		data = e.Data1
+	} else if c.LeftOperand == lang.Part2 {
+		data = e.Data2
+	} else {
+		fmt.Fprintf(os.Stderr, "This should never happen, you have found a bug")
+		os.Exit(1)
 	}
-	keyCode, ok := data1KeyCode[e.Data1]
-	if ok {
-		press.Press(keyCode)
+
+	switch c.Operator {
+	case lang.LessThanOperator:
+		return data < c.RightOperand
+	case lang.LessThanOrEqualToOperator:
+		return data <= c.RightOperand
+	case lang.EqualToOperator:
+		return data == c.RightOperand
+	case lang.UnequalToOperator:
+		return data != c.RightOperand
+	case lang.GreaterThanOrEqualToOperator:
+		return data >= c.RightOperand
+	case lang.GreaterThanOperator:
+		return data >= c.RightOperand
+	default:
+		fmt.Fprintf(os.Stderr, "This should never happen, you have found a bug")
+		os.Exit(1)
+		return false // Although, this line cannot be run it is required for the code to compile.
+	}
+}
+
+// doesMatcherMatchEvent returns true if m matches e, otherwise it returns false.
+func doesMatcherMatchEvent(m lang.Matcher, e portmidi.Event) bool {
+	return evaluateComparison(m.LeftComparison, e) && evaluateComparison(m.RightComparison, e)
+}
+
+func mapMidiEventToKeyPress(mappings []lang.Mapping, e portmidi.Event) {
+	for _, m := range mappings {
+		if doesMatcherMatchEvent(m.Matcher, e) {
+			press.Press(m.KeyCode)
+		}
 	}
 }
