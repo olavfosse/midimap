d2a5b75e08866f21a06378787a9989544f0f876a
Olav Fosse <mail@olavfosse.no>
2021-01-19 12:19:21 +0100

    Integrate lang with lang/matcher

--- lang/lang.go
+++ lang/lang.go
@@ -9,215 +9,53 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"./helper"
+	"./matcher"
 )
 
-type ComparisonOperator int
-
-const (
-	LessThanOperator ComparisonOperator = iota
-	LessThanOrEqualToOperator
-	EqualToOperator
-	UnequalToOperator
-	GreaterThanOrEqualToOperator
-	GreaterThanOperator
-)
-
-type Data1OrData2 int
-
-const (
-	Data1 Data1OrData2 = iota
-	Data2
-)
-
-type Comparison struct {
-	LeftOperand  Data1OrData2
-	Operator     ComparisonOperator
-	RightOperand int64
-}
-
-// skipToNonSpaceCharacter moves the start of *s to the next non-space character.
-// If there is a non-space character in *s, *s is modified such that the first character in *s is that non-space character, and true is returned.
-// Otherwise *s is not modified and false is returned.
-func skipToNonSpaceCharacter(s *string) (ok bool) {
-	for i, c := range *s {
-		if c != ' ' {
-			*s = (*s)[i:]
-			return true
-		}
-	}
-	return false
-}
-
-// parseComparison parses a comparison as specified in Section 1.2.1.1 COMPARISONS of the midimap-lang specification.
-// If s is a valid comparison as described by the specification, parseComparison returns comparison, true.
-// Otherwise parseComparison returns comparison, false.
-// s may not contain any leading or trailing space.
-func parseComparison(s string) (comparison Comparison, err error) {
-	unParsed := s // the characters of s which are yet to be parsed
-
-	switch {
-	case strings.HasPrefix(unParsed, "data1"):
-		comparison.LeftOperand = Data1
-	case strings.HasPrefix(unParsed, "data2"):
-		comparison.LeftOperand = Data2
-	default:
-		err = fmt.Errorf("comparison %q: no valid left operand", s)
-		return
-	}
-	unParsed = unParsed[len("datax"):] // Discard parsed leftOperand
-
-	skipToNonSpaceCharacter(&unParsed)
-	var operatorLength int
-	switch {
-	case strings.HasPrefix(unParsed, "=="):
-		comparison.Operator = EqualToOperator
-		operatorLength = 2
-	case strings.HasPrefix(unParsed, "!="):
-		comparison.Operator = UnequalToOperator
-		operatorLength = 2
-	case strings.HasPrefix(unParsed, "<="):
-		comparison.Operator = LessThanOrEqualToOperator
-		operatorLength = 2
-	case strings.HasPrefix(unParsed, ">="):
-		comparison.Operator = GreaterThanOrEqualToOperator
-		operatorLength = 2
-	case strings.HasPrefix(unParsed, "<"):
-		comparison.Operator = LessThanOperator
-		operatorLength = 1
-	case strings.HasPrefix(unParsed, ">"):
-		comparison.Operator = GreaterThanOperator
-		operatorLength = 1
-	default:
-		err = fmt.Errorf("comparison %q: no valid operator", s)
-		return
-	}
-	unParsed = unParsed[operatorLength:] // Discard parsed operator
-
-	skipToNonSpaceCharacter(&unParsed)
-	n, err := strconv.ParseInt(unParsed, 10, 64)
-	if err != nil {
-		err = fmt.Errorf("comparison %q: no valid right operand", s)
-		return
-	}
-	comparison.RightOperand = int64(n)
-
-	return
-}
-
-type LogicalOperator int
-
-const (
-	LogicalAndOperator LogicalOperator = iota
-	LogicalOrOperator
-	NoLogicalOperator LogicalOperator = -1
-)
-
-type Matcher struct {
-	LeftComparison  Comparison
-	Operator        LogicalOperator
-	RightComparison Comparison
-}
-
-// beforeAndAfter splits a string into two substrings, one before the leftmost match of a regexp and the other after it.
-// If r matches s, beforeAndAfter returns (left, right, true) where left is the characters prior to the leftmost match and right is the characters to the right of the leftmost match.
-// Otherwise, that is if r does not match s, ("", "", false) is returned.
-func beforeAndAfter(r *regexp.Regexp, s string) (string, string, bool) {
-	loc := r.FindStringIndex(s)
-	if loc == nil {
-		return "", "", false
-	}
-
-	before, after := s[:loc[0]], s[loc[1]:]
-	return before, after, true
-
-}
-
-// beforeAndAfterLogicalOperator splits a string into two substrings, one before a logical operator and the other after it.
-// If s contains "&&" beforeAndAfterLogicalOperator returns before, after, LogicalAndOperator, where before is the substring of s which appear before "&&" and after is the substring of s which appear after "&&".
-// Otherwise if s contains "||" beforeAndAfterLogicalOperator returns before, after, LogicalOrOperator, where before is the substring of s which appear before "||" and after is the substring of s which appear after it.
-// If s contains neither "||" nor "&&", beforeAndAfterLogicalOperator returns "", "", NoLogicalOperator
-func beforeAndAfterLogicalOperator(s string) (string, string, LogicalOperator) {
-	logicalAndRegexp := regexp.MustCompilePOSIX("&&")
-	logicalOrRegexp := regexp.MustCompilePOSIX(`\|\|`)
-
-	before, after, ok := beforeAndAfter(logicalAndRegexp, s)
-	if ok {
-		return before, after, LogicalAndOperator
-	}
-
-	before, after, ok = beforeAndAfter(logicalOrRegexp, s)
-	if ok {
-		return before, after, LogicalOrOperator
-	}
-
-	return "", "", NoLogicalOperator
-}
-
-// ParseMatcher parses a matcher as specified in Section 1.2.1 MATCHERS of the midimap-lang specification.
-// If s is a valid matcher as described by the specification, ParseMatcher returns matcher, true.
-// Otherwise ParseMatcher returns matcher, false.
-// s may not contain any leading or trailing space.
-func ParseMatcher(s string) (Matcher, error) {
-	var matcher Matcher
-
-	var left, right string
-	left, right, matcher.Operator = beforeAndAfterLogicalOperator(s)
-	if matcher.Operator == NoLogicalOperator {
-		return matcher, fmt.Errorf("matcher %q: no valid logical operator", s)
-	}
-
-	var err error
-	matcher.LeftComparison, err = parseComparison(strings.TrimSpace(left))
-	if err != nil {
-		return matcher, err
-	}
-	matcher.RightComparison, err = parseComparison(strings.TrimSpace(right))
-	if err != nil {
-		return matcher, err
-	}
-
-	return matcher, err
-}
-
-// parseKeyCode parses a KEYCODE as specified in Section 1.2.2 KEYCODES of the midimap-lang specification.
-// If s is a valid KEYCODE as described by the specification, parseKeyCode returns keyCode, true.
-// Otherwise parseKeyCode returns keyCode, false.
+// parseKeycode parses a keycode as specified in Section 1.2.2 KEYCODES of the midimap-lang specification.
+//
+// If s is a valid keycode as described by the specification, parseKeycode returns keycode, nil.
+// Otherwise, parseKeycode returns an error describing why the keycode is invalid.
 // s may not contain any leading or trailing spaces.
-func parseKeyCode(s string) (int, error) {
-	keyCode, err := strconv.Atoi(s)
+func parseKeycode(s string) (int, error) {
+	keycode, err := strconv.Atoi(s)
 	if err != nil {
-		return keyCode, fmt.Errorf("key code %q: invalid", s)
+		return keycode, fmt.Errorf("keycode %q: invalid", s)
 	}
-	return keyCode, nil
+	return keycode, nil
 }
 
 type Mapping struct {
-	Matcher Matcher
-	KeyCode int
+	Matcher matcher.Matcher
+	Keycode int
 }
 
 // parseMapping parses a mapping as specified in Section 1.2 MAPPINGS of the midimap-lang specification.
+//
 // If s is a valid mapping as described by the specification, parseMapping returns mapping, nil.
-// Otherwise parseMapping returns mapping, error.
+// Otherwise, parseMapping returns an error describing why the mapping is invalid.
 func parseMapping(s string) (mapping Mapping, err error) {
 	r := regexp.MustCompilePOSIX("->")
-	before, after, ok := beforeAndAfter(r, s)
+	before, after, ok := helper.BeforeAndAfter(r, s)
 	if !ok {
 		err = fmt.Errorf("mapping %q: no valid separator", s)
 		return
 	}
-	mapping.Matcher, err = ParseMatcher(strings.TrimSpace(before))
+	mapping.Matcher, err = matcher.Parse(strings.TrimSpace(before))
 	if err != nil {
 		return
 	}
-	mapping.KeyCode, err = parseKeyCode(strings.TrimSpace(after))
+	mapping.Keycode, err = parseKeycode(strings.TrimSpace(after))
 	return
 }
 
-// NextMapping attemps to parse the next MAPPING, as specified in Section 1.2 MAPPINGS of the midimap-lang specification, from r by parsing lines until a mapping is reached or an io error occurs.
-// If an io error occured NextMapping returns mapping, ioError.
-// If an invalid mapping is reached NextMapping returns mapping, err, where err is an error describing how the mapping is invalid.
-// Otherwise NextMapping returns mapping, nil.
+// NextMapping parses a mapping as specified in Section 1.2 MAPPINGS of the midimap-lang specification, by parsing lines until a mapping is reached or an io error occurs.
+//
+// If an io error occurs, NextMapping returns the io error.
+// If a parsing error occurs, NextMapping returns the parsing error.
+// Otherwise, NextMapping returns mapping, nil.
 func NextMapping(r *bufio.Reader) (mapping Mapping, err error) {
 	var line string
 	for {
@@ -233,8 +71,5 @@ func NextMapping(r *bufio.Reader) (mapping Mapping, err error) {
 		}
 	}
 
-	// I wonder if there is an idiom to return all the return values of a called function.
-	// It sounds a bit sugarish, so probably not.
-	mapping, err = parseMapping(line)
-	return
+	return parseMapping(line)
 }
--- lang/lang_test.go
+++ lang/lang_test.go
@@ -3,285 +3,87 @@ package lang
 import (
 	"fmt"
 	"testing"
+
+	"./matcher"
 )
 
-/*
- * parseComparison tests
- */
-
-// Test that parseComparison parses a valid comparison correctly.
-func TestParseComparisonValid(t *testing.T) {
-	wantedLeftOperand := Data1
-	wantedOperator := GreaterThanOperator
-	var wantedRightOperand int64 = 123
-
-	s := "data1 > 123"
-	comparison, err := parseComparison(s)
-
-	if comparison.LeftOperand != wantedLeftOperand {
-		t.Errorf("parseComparison returns a comparison with an incorrect leftOperand %v, want %v", comparison.LeftOperand, wantedLeftOperand)
-	}
-	if comparison.Operator != wantedOperator {
-		t.Errorf("parseComparison returns a comparison with an incorrect operator %v, want %v", comparison.Operator, wantedOperator)
-	}
-	if comparison.RightOperand != wantedRightOperand {
-		t.Errorf("parseComparison returns a comparison with an incorrect rightOperand %d, want %d", comparison.RightOperand, wantedRightOperand)
-	}
-	if err != nil {
-		t.Errorf("parseComparison returns non-nil err %q", err)
-	}
-}
-
-// Test that parseComparison parses an invalid comparison, lacking a left operand, correctly.
-func TestParseComparisonInvalidNoLeftOperand(t *testing.T) {
-	s := " > 123"
-
-	var wantedErr error = fmt.Errorf("comparison %q: no valid left operand", s)
-
-	_, err := parseComparison(s)
-
-	if err == nil {
-		t.Errorf("parseComparison returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseComparison returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that parseComparison parses an invalid comparison, lacking a operator, correctly.
-func TestParseComparisonInvalidNoOperator(t *testing.T) {
-	s := "data1 123"
-
-	var wantedErr error = fmt.Errorf("comparison %q: no valid operator", s)
-
-	_, err := parseComparison(s)
-
-	if err == nil {
-		t.Errorf("parseComparison returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseComparison returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that parseComparison parses an invalid comparison, lacking a left operand, correctly.
-func TestParseComparisonInvalidNoRightOperand(t *testing.T) {
-	s := "data1 >"
-
-	var wantedErr error = fmt.Errorf("comparison %q: no valid right operand", s)
-
-	_, err := parseComparison(s)
-
-	if err == nil {
-		t.Errorf("parseComparison returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseComparison returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-/*
- * ParseMatcher tests
- */
-
-func areComparisonsEqual(left, right Comparison) bool {
-	return left.LeftOperand == right.LeftOperand && left.Operator == right.Operator && left.RightOperand == right.RightOperand
-}
-
-// Test that ParseMatcher parses a valid matcher correctly.
-func TestParseMatcherValid(t *testing.T) {
-	wantedLeftComparison := Comparison{
-		LeftOperand:  Data1,
-		Operator:     EqualToOperator,
-		RightOperand: 557,
-	}
-	wantedOperator := LogicalAndOperator
-	wantedRightComparison := Comparison{
-		LeftOperand:  Data2,
-		Operator:     UnequalToOperator,
-		RightOperand: 365,
-	}
-
-	s := "data1 == 557 && data2 != 365"
-	matcher, err := ParseMatcher(s)
-
-	if !areComparisonsEqual(matcher.LeftComparison, wantedLeftComparison) {
-		t.Errorf("ParseMatcher returns a matcher with an incorrect LeftComparison %v, want %v", matcher.LeftComparison, wantedLeftComparison)
-	}
-	if matcher.Operator != wantedOperator {
-		t.Errorf("ParseMatcher returns a matcher with an incorrect Operator %d, want %d", matcher.Operator, wantedOperator)
-	}
-	if !areComparisonsEqual(matcher.RightComparison, wantedRightComparison) {
-		t.Errorf("ParseMatcher returns a matcher with an incorrect RightComparison %v, want %v", matcher.RightComparison, wantedRightComparison)
-	}
-	if err != nil {
-		t.Errorf("ParseMatcher returns non-nil err %q", err)
-	}
-}
-
-// Test that ParseMatcher parses an invalid matcher, with an invalid left comparison, correctly.
-func TestParseMatcherInvalidInvalidLeftComparison(t *testing.T) {
-	leftComparison := "data1 557"
-	wantedErr := fmt.Errorf("comparison %q: no valid operator", leftComparison)
-
-	s := leftComparison + " && data2 != 365"
-	_, err := ParseMatcher(s)
-
-	if err == nil {
-		t.Errorf("ParseMatcher returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("ParseMatcher returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that ParseMatcher parses an invalid matcher, lacking a logical operator, correctly.
-func TestParseMatcherInvalidNoLogicalOperator(t *testing.T) {
-	s := "data1 557 & & data2 != 365"
-
-	wantedErr := fmt.Errorf("matcher %q: no valid logical operator", s)
-
-	_, err := ParseMatcher(s)
-
-	if err == nil {
-		t.Errorf("ParseMatcher returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("ParseMatcher returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that ParseMatcher parses an invalid matcher, with an invalid right comparison, correctly.
-func TestParseMatcherInvalidInvalidRightComparison(t *testing.T) {
-	rightComparison := "data1 557"
-	wantedErr := fmt.Errorf("comparison %q: no valid operator", rightComparison)
-
-	s := "data2 != 365 && " + rightComparison
-	_, err := ParseMatcher(s)
-
-	if err == nil {
-		t.Errorf("ParseMatcher returns incorrect err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("ParseMatcher returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-/*
- * parseKeyCode tests
- */
-
-// Test that parseKeyCode parses a valid key code correctly.
-func TestParseKeyCodeValid(t *testing.T) {
-	wantedKeyCode := 1
+// Test that parseKeycode parses a simple valid keycode correctly.
+func TestParseKeycode(t *testing.T) {
+	var wantedErr error = nil
+	wantedKeycode := 1
 
 	s := "1" // ESC
+	keycode, err := parseKeycode(s)
 
-	keyCode, err := parseKeyCode(s)
-
-	if keyCode != wantedKeyCode {
-		t.Errorf("parseKeyCode returns incorrect key code %d, want %d", keyCode, wantedKeyCode)
+	if err != wantedErr {
+		t.Errorf("parseKeycode(%q) returns an incorrect error %q, want %v.", s, err, wantedErr)
 	}
-	if err != nil {
-		t.Errorf("parseKeyCode returns non-nil err %q", err)
+
+	if keycode != wantedKeycode {
+		t.Errorf("parseKeycode(%q) returns an incorrect keycode %d, want %d.", s, keycode, wantedKeycode)
 	}
 }
 
-// Test that parseKeyCode parses an invalid keyCode, with spaces interspersed between the digits, correctly.
-func TestParseKeyCodeInvalidInterspersedSpaces(t *testing.T) {
+// Test that parseKeycode parses a keycode, with spaces interspersed between the digits, correctly.
+func TestParseKeycodeInterspersedSpaces(t *testing.T) {
 	s := "1 2 3 4 5 6 7 8 9"
+	wantedErr := fmt.Errorf("keycode %q: invalid", s)
 
-	wantedErr := fmt.Errorf("key code %q: invalid", s)
-
-	_, err := parseKeyCode(s)
+	_, err := parseKeycode(s)
 
 	if err == nil {
-		t.Errorf("parseKeyCode returns incorrect err nil, want %q", wantedErr)
+		t.Errorf("parseKeycode(%q) returns an incorrect error %v, want %q.", s, err, wantedErr)
 	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseKeyCode returns incorrect err %q, want %q", err, wantedErr)
+		t.Errorf("parseKeycode(%q) returns an incorrect error %q, want %q.", s, err, wantedErr)
 	}
 }
 
-/*
- * parseMapping tests
- */
-
-func areMatchersEqual(left, right Matcher) bool {
-	areLeftComparisonsEqual := areComparisonsEqual(left.LeftComparison, right.LeftComparison)
-	areOperatorsEqual := left.Operator == right.Operator
-	areRightComparisonsEqual := areComparisonsEqual(left.RightComparison, right.RightComparison)
-	return areLeftComparisonsEqual && areOperatorsEqual && areRightComparisonsEqual
+func (m Mapping) equal(n Mapping) bool {
+	return m.Matcher.Equal(n.Matcher) && m.Keycode == n.Keycode
 }
 
-// Test that parseMapping parses a valid mapping correctly.
-func TestParseMappingValid(t *testing.T) {
+// Test that parseMapping parses a simple valid mapping correctly.
+func TestParseMapping(t *testing.T) {
+	var wantedErr error = nil
+	wantedMapping := Mapping{
+		Matcher: matcher.MatcherWithLogicalOperator{
+			LeftMatcher: matcher.MatcherWithoutLogicalOperator{
+				LeftOperand:  matcher.Data1,
+				Operator:     matcher.EqualToOperator,
+				RightOperand: 44,
+			},
+			Operator: matcher.LogicalAndOperator,
+			RightMatcher: matcher.MatcherWithoutLogicalOperator{
+				LeftOperand:  matcher.Data2,
+				Operator:     matcher.EqualToOperator,
+				RightOperand: 64,
+			},
+		},
+		Keycode: 1,
+	}
+
 	s := "data1 == 44 && data2 == 64 -> 1"
-
-	wantedMatcher := Matcher{
-		LeftComparison: Comparison{
-			LeftOperand:  Data1,
-			Operator:     EqualToOperator,
-			RightOperand: 44,
-		},
-		Operator: LogicalAndOperator,
-		RightComparison: Comparison{
-			LeftOperand:  Data2,
-			Operator:     EqualToOperator,
-			RightOperand: 64,
-		},
-	}
-	wantedKeyCode := 1
-
 	mapping, err := parseMapping(s)
 
-	if !areMatchersEqual(mapping.Matcher, wantedMatcher) {
-		t.Errorf("parseMapping returns mapping with incorrect matcher %v, want %v", mapping.Matcher, wantedMatcher)
+	if err != wantedErr {
+		t.Errorf("parseMapping(%q) returns an incorrect error %q, want %v.", s, err, wantedErr)
 	}
-	if mapping.KeyCode != wantedKeyCode {
-		t.Errorf("parseMapping returns mapping with incorrect key code %d, want %d", mapping.KeyCode, wantedKeyCode)
-	}
-	if err != nil {
-		t.Errorf("parseMapping returns non-nil error %q", err)
+
+	if !mapping.equal(wantedMapping) {
+		t.Errorf("parseMapping(%q) returns an incorrect mapping %v, want %v.", s, mapping, wantedMapping)
 	}
 }
 
-// Test that parseMapping parses an invalid mapping, with an invalid matcher, correctly.
-func TestParseMappingInvalidInvalidMatcher(t *testing.T) {
-	matcher := "data1 < 44 data2 == 64"
-	s := matcher + " -> 1"
-
-	wantedErr := fmt.Errorf("matcher %q: no valid logical operator", matcher)
-
-	_, err := parseMapping(s)
-
-	if err == nil {
-		t.Errorf("parseMapping returns err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseMapping returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that parseMapping parses an invalid mapping, with an invalid separator, correctly.
-func TestParseMappingInvalidInvalidSeparator(t *testing.T) {
+// Test that parseMapping parses a mapping, lacking a valid separator, correctly.
+func TestParseMappingInvalidSeparator(t *testing.T) {
 	s := "data1 < 44 && data2 == 64 - > 12" // interspersing spaces in the separator is not allowed.
-
 	wantedErr := fmt.Errorf("mapping %q: no valid separator", s)
 
 	_, err := parseMapping(s)
 
 	if err == nil {
-		t.Errorf("parseMapping returns err nil, want %q", wantedErr)
+		t.Errorf("parseMapping(%q) returns an incorrect error %v, want %q.", s, err, wantedErr)
 	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseMapping returns incorrect err %q, want %q", err, wantedErr)
-	}
-}
-
-// Test that parseMapping parses an invalid mapping, with an invalid keycode, correctly.
-func TestParseMappingInvalidInvalidKeycode(t *testing.T) {
-	keyCode := "1 2"
-	s := "data1 < 44 && data2 == 64 -> " + keyCode
-
-	wantedErr := fmt.Errorf("key code %q: invalid", keyCode)
-
-	_, err := parseMapping(s)
-
-	if err == nil {
-		t.Errorf("parseMapping returns err nil, want %q", wantedErr)
-	} else if err.Error() != wantedErr.Error() {
-		t.Errorf("parseMapping returns incorrect err %q, want %q", err, wantedErr)
+		t.Errorf("parseMapping(%q) returns an incorrect error %q, want %q.", s, err, wantedErr)
 	}
 }
