404248a16953aeff886f621f704f5246230ef05c
Olav Fosse <mail@olavfosse.no>
2021-01-19 16:31:42 +0100

    Integrate lang/matcher and updated lang package with midimap.go

--- midimap.go
+++ midimap.go
@@ -15,6 +15,7 @@ import (
 
 	"./driver"
 	"./lang"
+	"./lang/matcher"
 )
 
 func main() {
@@ -38,16 +39,16 @@ func dispatch(args []string) error {
 			return err
 		}
 
-		var matcher lang.Matcher
+		var m matcher.Matcher
 		receivedMatcher := false
 		if len(args) == 4 {
-			matcher, err = lang.ParseMatcher(args[3])
+			m, err = matcher.Parse(args[3])
 			if err != nil {
 				return err
 			}
 			receivedMatcher = true
 		}
-		return dispatchLog(portNumber, receivedMatcher, matcher)
+		return dispatchLog(portNumber, receivedMatcher, m)
 	case len(args) == 4 && args[1] == "map":
 		portNumber, err := parsePortNumber(args[2])
 		if err != nil {
@@ -85,7 +86,7 @@ func dispatchPorts() error {
 }
 
 // dispatchLog logs incoming MIDI events from the port with the number portNumber.
-func dispatchLog(portNumber uint64, receivedMatcher bool, matcher lang.Matcher) error {
+func dispatchLog(portNumber uint64, receivedMatcher bool, matcher matcher.Matcher) error {
 	drv, err := driver.New()
 	if err != nil {
 		return err
@@ -111,7 +112,7 @@ func dispatchLog(portNumber uint64, receivedMatcher bool, matcher lang.Matcher) 
 	rd := reader.New(
 		reader.NoLogger(),
 		reader.Each(func(pos *reader.Position, msg midi.Message) {
-			if !receivedMatcher || doesMatcherMatchMessage(matcher, msg) {
+			if !receivedMatcher || matcherMatchesMessage(matcher, msg) {
 				fmt.Println("---===---")
 				fmt.Printf("%s\n", msg)
 				fmt.Printf("status: %d\n", msg.Raw()[0])
@@ -233,54 +234,61 @@ func getMappingsFromMapName(mapName string) (mappings []lang.Mapping, err error)
 
 func mapMIDIMessageToKeyPress(kb keybd_event.KeyBonding, mappings []lang.Mapping, msg midi.Message) (err error) {
 	for _, mapping := range mappings {
-		if doesMatcherMatchMessage(mapping.Matcher, msg) {
-			err = press(kb, mapping.KeyCode)
-			return
+		if matcherMatchesMessage(mapping.Matcher, msg) {
+			// NB: We iterate through all mappings regardless of if some earlier mapping matched. This is expected behaviour.
+			err = press(kb, mapping.Keycode)
+			if err != nil {
+				break
+			}
 		}
 	}
 	return
 }
 
-// doesMatcherMatchMessage returns true if m matches msg, otherwise it returns false.
-func doesMatcherMatchMessage(m lang.Matcher, msg midi.Message) bool {
-	if m.Operator == lang.LogicalOrOperator {
-		return evaluateComparison(m.LeftComparison, msg) || evaluateComparison(m.RightComparison, msg)
-	}
-	return evaluateComparison(m.LeftComparison, msg) && evaluateComparison(m.RightComparison, msg)
-}
+// matcherMatchesMessage reports whether msg matches m.
+func matcherMatchesMessage(m matcher.Matcher, msg midi.Message) bool {
+	switch m := m.(type) {
+	case matcher.MatcherWithoutLogicalOperator:
+		var data int64
+		// msg.Raw()[0] is status
+		// msg.Raw()[1] is data1
+		// msg.Raw()[2] is data2
+		switch m.LeftOperand {
+		case matcher.Data1:
+			data = int64(msg.Raw()[1])
+		case matcher.Data2:
+			data = int64(msg.Raw()[2])
+		default:
+			panic("unreachable")
+		}
 
-// evaluateComparison returns true if the comparison c evaluates to true in the context of event e in midimap-lang. Otherwise it returns false.
-func evaluateComparison(c lang.Comparison, msg midi.Message) bool {
-	var data int64
-	// msg.Raw()[0] is status
-	// msg.Raw()[1] is data1
-	// msg.Raw()[2] is data2
-	if c.LeftOperand == lang.Data1 {
-		data = int64(msg.Raw()[1])
-	} else if c.LeftOperand == lang.Data2 {
-		data = int64(msg.Raw()[2])
-	} else {
-		fmt.Fprintf(os.Stderr, "This should never happen, you have found a bug")
-		os.Exit(1)
-	}
-
-	switch c.Operator {
-	case lang.LessThanOperator:
-		return data < c.RightOperand
-	case lang.LessThanOrEqualToOperator:
-		return data <= c.RightOperand
-	case lang.EqualToOperator:
-		return data == c.RightOperand
-	case lang.UnequalToOperator:
-		return data != c.RightOperand
-	case lang.GreaterThanOrEqualToOperator:
-		return data >= c.RightOperand
-	case lang.GreaterThanOperator:
-		return data >= c.RightOperand
+		switch m.Operator {
+		case matcher.LessThanOperator:
+			return data < m.RightOperand
+		case matcher.LessThanOrEqualToOperator:
+			return data <= m.RightOperand
+		case matcher.EqualToOperator:
+			return data == m.RightOperand
+		case matcher.UnequalToOperator:
+			return data != m.RightOperand
+		case matcher.GreaterThanOrEqualToOperator:
+			return data >= m.RightOperand
+		case matcher.GreaterThanOperator:
+			return data >= m.RightOperand
+		default:
+			panic("unreachable")
+		}
+	case matcher.MatcherWithLogicalOperator:
+		switch m.Operator {
+		case matcher.LogicalAndOperator:
+			return matcherMatchesMessage(m.LeftMatcher, msg) && matcherMatchesMessage(m.RightMatcher, msg)
+		case matcher.LogicalOrOperator:
+			return matcherMatchesMessage(m.LeftMatcher, msg) || matcherMatchesMessage(m.RightMatcher, msg)
+		default:
+			panic("unreachable")
+		}
 	default:
-		fmt.Fprintf(os.Stderr, "This should never happen, you have found a bug")
-		os.Exit(1)
-		return false // Although, this line cannot be run it is required for the code to compile.
+		panic("unreachable")
 	}
 }
 
